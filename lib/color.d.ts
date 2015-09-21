3114990114814cfaa3c7209fe32902a07507b22f
slikts <dabas@untu.ms>
2015-09-21 19:19:47 +0300

    typings

---
+++ lib/color.d.ts
@@ -0,0 +1,28 @@
+declare class Color {
+    private rgb;
+    constructor(rgb: number[]);
+    constructor(rgb: string);
+    mix(target: Color): Color;
+    add(target: Color): Color;
+    subtract(target: Color): Color;
+    toString(): string;
+    values(): number[];
+    static clamp(value: number, max?: number, min?: number): number;
+    static parse(color: string): number[];
+}
+declare namespace Color {
+    const WHITE: Color;
+    const BLACK: Color;
+    const RED: Color;
+    const GREEN: Color;
+    const BLUE: Color;
+    class ValueNumberError extends TypeError {
+        message: string;
+    }
+    class ValueTypeError extends TypeError {
+        message: string;
+    }
+    class InvalidAlphaError extends TypeError {
+        message: string;
+    }
+}
