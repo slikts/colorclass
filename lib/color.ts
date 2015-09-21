1e0a156d9522e97459b6fd29ea5db9b38908abf3
slikts <dabas@untu.ms>
2015-09-21 18:48:22 +0300

    Init

---
+++ lib/color.ts
@@ -0,0 +1,74 @@
+'use strict';
+
+class Color {
+  protected rgb: [number, number, number]
+
+  constructor(rgb: number[]);
+  constructor(rgb: string);
+  constructor(rgb: any) {
+    if (typeof rgb === 'string') {
+      rgb = Color.parse(rgb);
+    }
+    if (rgb.length !== 3) {
+      throw new Color.ValueNumberError;
+    }
+    if (rgb.some(isNaN)) {
+      throw new Color.ValueTypeError;
+    }
+    this.rgb = rgb;
+  }
+
+  mix(target: Color) {
+    return new Color(this.rgb.map((value, index) => {
+      return Math.round((value + target.rgb[index]) / 2);
+    }));
+  }
+
+  add(target: Color) {
+    return new Color(this.rgb.map((value, index) => {
+      return value + target.rgb[index];
+    }));
+  }
+
+  subtract(target: Color) {
+    return new Color(this.rgb.map((value, index) => {
+      return value - target.rgb[index];
+    }));
+  }
+
+  toString() {
+    return `rgb(${ this.values().join(',') })`;
+  }
+
+  values() {
+    return this.rgb.map((x) => { return Color.clamp(x); });
+  }
+
+  static clamp(value: number, max = 255, min = 0) {
+    return Math.min(max, Math.max(min, value));
+  }
+
+  static parse(color: string) {
+    return color.match(/\((.*)\)/)[1].split(',').map((x) => { return +x; });
+  }
+}
+
+namespace Color {
+  export const WHITE = new Color([255, 255, 255]);
+  export const BLACK = new Color([0, 0, 0]);
+  export const RED = new Color([255, 0, 0]);
+  export const GREEN = new Color([0, 255, 0]);
+  export const BLUE = new Color([0, 0, 255]);
+
+  export class ValueNumberError extends TypeError {
+    message = 'Wrong number of color values';
+  }
+
+  export class ValueTypeError extends TypeError {
+    message = 'Invalid color values';
+  }
+
+  export class InvalidAlphaError extends TypeError {
+    message = 'Invalid alpha value';
+  }
+}
---
+++ test/color.js
@@ -0,0 +1,52 @@
+describe('instance methods', function() {
+  it('should construct from rgb array', function() {
+    new Color([255, 0, 5]);
+  });
+  it('should construct from css rgb string', function() {
+    new Color('rgb(255, 0, 0)');
+  });
+  it('should not construct from more or less than 3 color values', function() {
+    expect(function() {
+      new Color([0, 1]);
+    }).toThrow(new Color.ValueNumberError);
+    expect(function() {
+      new Color([0, 1, 2, 3]);
+    }).toThrow(new Color.ValueNumberError);
+  });
+  it('should convert to string', function() {
+    expect(''+Color.RED).toBe('rgb(255,0,0)');
+    expect(Color.RED.toString()).toBe('rgb(255,0,0)');
+  });
+  it('should mix different colors', function() {
+    expect(Color.RED.mix(Color.BLUE)+'').toBe('rgb(128,0,128)');
+  });
+  it('should not construct from invalid values', function() {
+    expect(function() {
+      new Color('rgb(255, 0, a)');
+    }).toThrow(new Color.ValueTypeError);
+  });
+  it('should clamp values to 0-255', function() {
+    expect(new Color([-10,500,128]).values()).toEqual([0, 255, 128]);
+  });
+  it('should add different colors', function() {
+    expect(Color.BLUE.add(Color.RED).values()).toEqual([255, 0, 255]);
+    expect(Color.RED.add(new Color([-128, 32, 0])).values()).toEqual([127, 32, 0]);
+  });
+  it('should subtract different colors', function() {
+    expect(Color.RED.subtract(new Color([128, 0, 0])).values()).toEqual([127, 0, 0]);
+  });
+});
+
+describe('class methods', function() {
+  it('should clamp values from 0 - 255', function() {
+    expect(Color.clamp(256)).toBe(255);
+    expect(Color.clamp(-1)).toBe(0);
+    expect(Color.clamp(128)).toBe(128);
+  });
+  it('should parse css rgb() colors', function() {
+    expect(Color.parse('rgb(0,0,0)')).toEqual([0, 0, 0]);
+  });
+  it('should parse invalid css rgb() colors to NaN', function() {
+    expect(Color.parse('rgb(a,b,c)')).toEqual([NaN, NaN, NaN]);
+  });
+});
